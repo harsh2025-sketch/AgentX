e72946ab3afdc8d9a4a8b9aac86925531c2ad1b7
Harsh Dinodia <ghosttalk719@gmail.com>
2026-09-15 23:50:36 +0530

    style: format AX-124 no-broadening proof

--- tests/unit/test_scope_no_implicit_broadening.py
+++ tests/unit/test_scope_no_implicit_broadening.py
@@ -7,9 +7,7 @@ from agentx.hive.scope_retrieval import GlobalKnowledgePolicy, ScopedKnowledgeQu
 
 
 def test_scope_query_has_no_string_or_boolean_escape_hatch() -> None:
-    query = ScopedKnowledgeQuery(
-        scope=KnowledgeScope({ScopeDimension.PROJECT: "agentx"})
-    )
+    query = ScopedKnowledgeQuery(scope=KnowledgeScope({ScopeDimension.PROJECT: "agentx"}))
     assert query.global_policy is GlobalKnowledgePolicy.EXCLUDE
     assert not hasattr(query, "scope_text")
     assert not hasattr(query, "allow_all")
