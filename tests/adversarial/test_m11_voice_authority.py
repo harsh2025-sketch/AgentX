f5da84fa8457ef8a1276600c1d2f09d2c56675c1
Harsh Dinodia <ghosttalk719@gmail.com>
2026-09-18 21:18:15 +0530

    style(m11): match canonical import ordering

--- tests/adversarial/test_m11_voice_authority.py
+++ tests/adversarial/test_m11_voice_authority.py
@@ -7,6 +7,11 @@ from decimal import Decimal
 from uuid import uuid4
 
 import pytest
+from tests.support.demo_capability import (
+    HostileMetadataCapability,
+    NoteWriteParams,
+    hostile_request,
+)
 
 from agentx.capabilities.executor import Executor
 from agentx.capabilities.human_approval import HumanApprovalRequest
@@ -23,11 +28,6 @@ from agentx.voice_runtime import (
     PendingVoiceConfirmation,
     SpokenConfirmationProtocol,
     VoiceGovernedActionBridge,
-)
-from tests.support.demo_capability import (
-    HostileMetadataCapability,
-    NoteWriteParams,
-    hostile_request,
 )
 
 
